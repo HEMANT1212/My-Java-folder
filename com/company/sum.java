4b2ab1833ce7e3e84f04591e2749033016c77d09
HEMANT1212 <rajputanashan98@gmail.com>
2024-01-26 09:24:12 +0530

    first commit

---
+++ com/company/sum.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class sum {
+    public static void main(String[] args) {
+        int a = 10;
+        int n= 20;
+        int d = 30;
+        int sum = a+n+d;
+        System.out.println("the sum of three numbers");
+        System.out.println(sum);
+    }
+    
+}
